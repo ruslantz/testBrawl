37fad1afb8ed5298742f83f4cdf9a5d6749bd92f
ruslantz <antz85@mail.ru>
2023-05-22 14:22:45 +0300

    new

---
+++ index.js
@@ -0,0 +1,22 @@
+document.getElementById("input").addEventListener("input", function() {
+    if (this.value === "тест1") {
+        document.getElementById("img1").classList.remove("hidden");
+    }
+    if (this.value === "тест2") {
+        document.getElementById("img2").classList.remove("hidden");
+    }
+    if (this.value === "тест3") {
+        document.getElementById("img3").classList.remove("hidden");
+    }
+    if (this.value === "тест4") {
+        document.getElementById("img4").classList.remove("hidden");
+    }
+    if (this.value === "тест5") {
+        document.getElementById("img5").classList.remove("hidden");
+    }
+    if (this.value === "тест6") {
+        document.getElementById("img6").classList.remove("hidden");
+    }
+})
+
+    
