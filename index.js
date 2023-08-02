ab046e8055b2886d2393ce4f1589f40bd7e4c4b3
Ruslan <antz85@mail.ru>
2023-08-03 00:31:09 +0300

    добавляет сохранение в локальное хранилище

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 import { USED, POWER } from './data.js';
+import { QUANTITY } from './data.js';
+import { renderPicture, restoreDataFromLocalStorageHTML } from './render.js';
+import { createPictures } from './data.js';
 
+// Функция для сохранения данных в локальное хранилище
+function saveDataToLocalStorage() {
+    localStorage.setItem("usedWords", JSON.stringify(USED));
+}
+
+// Функция для восстановления данных из локального хранилища
+function restoreDataFromLocalStorage() {
+    const usedWordsJSON = localStorage.getItem("usedWords");
+    if (usedWordsJSON) {
+        USED.push(...JSON.parse(usedWordsJSON));
+    }
+}
+
+const pictures = createPictures(QUANTITY);
+
+// Восстанавливаем данные из локального хранилища при загрузке страницы
+window.onload = function () {
+    restoreDataFromLocalStorage();
+    restoreDataFromLocalStorageHTML();
+};
 
 document.getElementById("input").addEventListener("input", function () {
+    let element = pictures.find(item => item.power === this.value);
     if (POWER.includes(this.value)) {
         if (USED.includes(this.value)) {
             console.log("Слово повторяется: ");
         } else {
             USED.push(this.value);
             console.log("Слово найдено: ");
+            renderPicture(element);
+            // Сохраняем данные в локальное хранилище после каждого изменения
+            saveDataToLocalStorage();
         }
     }
-}
-)
+});
--- render.js
+++ render.js
@@ -1,20 +1,36 @@
-import { USED } from './data.js';
-
 const picturesContainer = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture')
   .content
   .querySelector('.picture');
 
-const createPicture = (picture) => {
+export const createPicture = (picture) => {
   const element = pictureTemplate.cloneNode(true);
   element.querySelector('.picture__img').src = picture.url;
 
   return element;
-  };
+};
 
 export const renderPicture = (id) => {
-    const picturesFragment = document.createDocumentFragment();
-    picturesFragment.append(createPicture(id));
-    return picturesContainer.append(picturesFragment);
-  };
+  const picturesFragment = document.createDocumentFragment();
+  picturesFragment.append(createPicture(id));
+  picturesContainer.append(picturesFragment);
+
+  // Сохранение данных в локальное хранилище
+  saveDataToLocalStorage();
+};
+
+// Функция для сохранения данных в локальное хранилище
+function saveDataToLocalStorage() {
+  const picturesHTML = picturesContainer.innerHTML;
+  localStorage.setItem("savedPicturesHTML", picturesHTML);
+}
+
+// Функция для восстановления данных из локального хранилища
+export const restoreDataFromLocalStorageHTML = () => {
+  const picturesHTML = localStorage.getItem("savedPicturesHTML");
+  if (picturesHTML) {
+    picturesContainer.innerHTML = picturesHTML;
+  }
+}
+
 
